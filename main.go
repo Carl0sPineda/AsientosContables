675d3e10f720a44cd01e684158fc3a0f58fe653e
Carl0sPineda <carlos7712p@gmail.com>
2024-08-05 10:06:14 -0600

    chore: Update options for the app

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -17,9 +18,12 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "AsientosContables",
-		Width:  1024,
-		Height: 768,
+		Title:  		    "",
+		Width:             800,
+		Height:            600,
+		MinWidth:          800,
+        MinHeight:         600,
+		WindowStartState:   options.Maximised,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
@@ -28,6 +32,12 @@ func main() {
 		Bind: []interface{}{
 			app,
 		},
+		Windows: &windows.Options{
+			DisableWindowIcon:                 true,
+        },
+		Debug: options.Debug{
+            OpenInspectorOnStartup: true,
+        },
 	})
 
 	if err != nil {
